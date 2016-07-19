574a068cd7b7bff61e991c2c23ae013267553baf
Arnav Sastry <arnav.sastry@addepar.com>
2016-07-18 17:16:49 -0700

    Moved template to correct location

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def accounts():
         accounts = cur.fetchall()
         cur.close()
 
-        return render_template('account_template.html', accounts=accounts)
+        return render_template('accounts.html', accounts=accounts)
     except Exception as e:
         return str(e)
 
