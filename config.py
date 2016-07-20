83b1dad26c056902de3b2b4c8c3c05f34046d30c
Arnav Sastry <arnav.sastry@addepar.com>
2016-07-20 10:18:16 -0700

    fleshed out mappings

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ class Config(object):
     FIRM_ID = 1
     FIRM_URL = 'https://terrabella.addepar.com'
 
+    # Need to have _VIEW at the end
     ACCOUNTS_VIEW = 228
     FINANCIAL_ACCOUNTS_VIEW = 229
-    FINANCIAL_HOLDINGS = 230
-    SECURITIES = 231
+    FINANCIAL_HOLDINGS_VIEW = 230
+    SECURITIES_VIEW = 231
--- mappings.py
+++ mappings.py
@@ -4,13 +4,10 @@ Each mapping object has a view name that corresponds to the table, a "columns"
 object which maps columns to entries in the CSV, a "constants" object which
 maps columns to constant values and a "unique" field which marks the column that is
 the unique identifier.
+
+ALERT: BE VERY CAREFUL WHEN EDITING THIS OBJECT. YOU COULD ACCIDENTALLY CREATE A SQL INJECTION!
 """
 mappings = {
-    # 'salesforce.finserv__financialaccount__c': {
-    #     'name': 'CLIENTS',
-    #     'columns': {},
-    #     'constants': {}
-    # },
     'salesforce.account': {
         'name': 'ACCOUNTS',
         'columns': {
@@ -22,7 +19,50 @@ mappings = {
             'recordtypeid': '01236000000NPpsAAG'
         },
         'unique': 'addepar_entity_id__c'
+    },
+    'salesforce.finserv__financialaccount__c': {
+        'name': 'FINANCIAL_ACCOUNTS',
+        'columns': {
+            'finserv__performanceqtd__c': 'Performance QTD',
+            'finserv__financialaccountnumber__c': 'Account Number',
+            'finserv__performancemtd__c': 'Performance MTD',
+            'finserv__balance__c': 'Balance',
+            'name': 'Financial Account',
+            'finserv__performance1yr__c': 'Performance 1YR',
+            'finserv__performanceytd__c': 'Performance YTD',
+            'finserv__performance3yr__c': 'Performance 3YR',
+            'addepar_entity_id__c': 'Financial Account [Entity ID]',
+            'FinServ__PrimaryOwner__r__Addepar_Entity_ID__c': 'Client [Entity ID]',
+            'FinServ__ServiceProvider__c': 'Financial Service'
+        },
+        'constants': {
+            'FinServ__Ownership__c': 'Individual',
+            'recordtypeid': '01236000000NPpsAAG'
+        },
+        'unique': 'addepar_entity_id__c'
+    },
+    'salesforce.finserv__financialholding__c': {
+        'name': 'FINANCIAL_HOLDINGS',
+        'columns': {
+            'finserv__primaryowner__r__addepar_entitiy_id__c': 'Client [Entity ID]',
+            'unfunded_commitments__c': 'Unfunded Commitments',
+            'addepar_position_id__c': 'Position [Position ID]',
+            'finserv__purchaseprice__c': 'Purchase Price'
+        },
+        'constants': {},
+        'unique': 'addepar_position_id__c'
+    },
+    'salesforce.finserv__securities__c': {
+        'name': 'SECURITIES',
+        'columns': {
+            'name': 'Ticker Symbol',
+            'finserv__cusip__c': 'CUSIP',
+            'addepar_entity_id__c': 'Security [Entity ID]',
+            'finserv__securityid__c': 'BBGID',
+            'finserv__price__c': 'Price',
+            'finserv__securitiesname__c': 'Security'
+        },
+        'constants': {},
+        'unique': 'addepar_entity_id__c'
     }
-    # 'finserv__financialholding__c': {},
-    # 'finserv__securities__c': {}
 }
