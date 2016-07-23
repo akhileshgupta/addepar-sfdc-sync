4737a496e9810be15c56c1f5cad5b8fc129b6bc0
Arnav Sastry <arnav.sastry@addepar.com>
2016-07-22 17:16:25 -0700

    Got rid of gunicorn

--- app.py
+++ app.py
@@ -105,7 +105,9 @@ def work():
 """Start the scheduled job"""
 if __name__ == '__main__':
     app = Flask(__name__)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
     schedule.every().hour.do(work)
 
     while True:
