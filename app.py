141db2eb5c2ed40d60ba9608f57891702db7701e
Arnav Sastry <arnav.sastry@addepar.com>
2016-07-22 17:57:01 -0700

    Remove flask

--- app.py
+++ app.py
@@ -104,9 +104,9 @@ def work():
 
 """Start the scheduled job"""
 if __name__ == '__main__':
-    app = Flask(__name__)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # app = Flask(__name__)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
 
     schedule.every().hour.do(work)
 
